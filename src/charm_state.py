8fe39fd32ee6d7ad3facdcd4ffca82559005d7ef
arturo-seijas <102022572+arturo-seijas@users.noreply.github.com>
2024-01-02 12:25:01 +0100

    Update license headers (#64)

--- src/charm_state.py
+++ src/charm_state.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-# Copyright 2023 Canonical Ltd.
+# Copyright 2024 Canonical Ltd.
 # See LICENSE file for licensing details.
 
 """Module defining the CharmState class which represents the state of the SAML Integrator charm."""
